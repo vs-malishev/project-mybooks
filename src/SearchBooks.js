768742cb6e497b9eb2ac84ee1b67e81a04e0ed4f
vmalishev <vsevolod.malishev@gmail.com>
2017-07-24 09:05:37 -0400

    cleaned up the app, added Books and SearchBooks components and routes

---
+++ src/SearchBooks.js
@@ -0,0 +1,43 @@
+import React, {Component} from 'react'
+import { Link } from 'react-router-dom'
+import PropTypes from 'prop-types'
+import escapeRegExp from 'escape-string-regexp'
+import sortBy from 'sort-by'
+
+class SearchBooks extends Component {
+	static PropTypes = {
+		results: PropTypes.array.isRequired,
+		onSearchSubmit: PropTypes.func.isRequired
+	}
+
+	render() {
+		return (
+			<div className="search-books">
+				<div className="search-books-bar">
+					<Link
+						to="/"
+						className="close-search"
+					>Close</Link>
+					<div className="search-books-input-wrapper">
+						{/*
+						NOTES: The search from BooksAPI is limited to a particular set of search terms.
+						You can find these search terms here:
+						https://github.com/udacity/reactnd-project-myreads-starter/blob/master/SEARCH_TERMS.md
+
+						However, remember that the BooksAPI.search method DOES search by title or author. So, don't worry if
+						you don't find a specific author or title. Every search is limited by search terms.
+						*/}
+						<input type="text" placeholder="Search by title or author"/>
+
+					</div>
+				</div>
+				<div className="search-books-results">
+					<ol className="books-grid"></ol>
+				</div>
+			</div>
+		)
+	}
+
+}
+
+export default SearchBooks
